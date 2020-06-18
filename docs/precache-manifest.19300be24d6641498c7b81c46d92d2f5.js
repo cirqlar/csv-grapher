8e26f77ea899cc65b3534ebca0dbffc8592cec1e
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-06-18 05:53:25 +0100

    Run build

---
+++ docs/precache-manifest.19300be24d6641498c7b81c46d92d2f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97ca6a444770c231ce3cbd661220c57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a66fa41619f041fbae7",
+    "url": "./static/js/2.e21954c6.chunk.js"
+  },
+  {
+    "revision": "79045ac582dee1f1c8efcfa12986f639",
+    "url": "./static/js/2.e21954c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2357b285ee014089511c",
+    "url": "./static/js/main.a72097bc.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3e688619afcb19ca46e8d251ecb55efa.js"
+  "./precache-manifest.19300be24d6641498c7b81c46d92d2f5.js"
 );
 
 self.addEventListener('message', (event) => {
